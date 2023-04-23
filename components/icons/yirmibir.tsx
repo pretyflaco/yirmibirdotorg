70a147d9adca45a121dff074905d7b67512766b0
pretyflaco <kemal@ideasarelikeflames.com>
2023-04-24 00:04:22 +0300

    fix svg logo

--- components/icons/yirmibir.tsx
+++ components/icons/yirmibir.tsx
@@ -1,151 +1,35 @@
 import React from 'react';
 export const Yirmibir: React.FC = () => {
     return (
-        <svg
-        width="317.5mm"
-        height="52.768761mm"
-        viewBox="0 0 317.49999 52.768759"
-        version="1.1"
-        id="svg5"
-        xmlns="http://www.w3.org/2000/svg">
-       <defs
-          id="defs2">
-         <clipPath
-            clipPathUnits="userSpaceOnUse"
-            id="clipPath958">
-           <rect
-              style="fill:#5f8dd3;stroke:#000000;stroke-width:1.165"
-              id="rect960"
-              width="143.21965"
-              height="32.282162"
-              x="43.607281"
-              y="221.28348" />
-         </clipPath>
-         <clipPath
-            clipPathUnits="userSpaceOnUse"
-            id="clipPath1070">
-           <rect
-              style="fill:#5f8dd3;stroke:#000000;stroke-width:1.165"
-              id="rect1072"
-              width="140.70998"
-              height="25.007889"
-              x="45.002094"
-              y="223.0201" />
-         </clipPath>
-       </defs>
-       <g id="layer1">
-         <rect
-            style="fill:#ffffff;stroke-width:1.58764"
-            id="rect110657"
-            width="317.5"
-            height="52.768761"
-            x="-2.220446e-16"
-            y="0" />
-         <text
-            style="font-size:71.3091px;line-height:1.25;font-family:'The Bold Font';-inkscape-font-specification:'The Bold Font';stroke-width:1.78273"
-            x="185.955"
-            y="52.554855"
-            id="text107199"><tspan
-              id="tspan107197"
-              style="stroke-width:1.78273"
-              x="185.955"
-              y="52.554855">B  </tspan></text>
-         <text
-            style="font-size:71.3091px;line-height:1.25;font-family:'The Bold Font';-inkscape-font-specification:'The Bold Font';stroke-width:1.78273"
-            x="69.28112"
-            y="52.412239"
-            id="text96253"><tspan
-              id="tspan96251"
-              style="stroke-width:1.78273"
-              x="69.28112"
-              y="52.412239">RM  </tspan></text>
-         <text
-            style="font-size:71.3091px;line-height:1.25;font-family:'The Bold Font';-inkscape-font-specification:'The Bold Font';stroke-width:1.78273"
-            x="0.1426184"
-            y="52.768764"
-            id="text35194"><tspan
-              id="tspan35192"
-              style="stroke-width:1.78273"
-              x="0.1426184"
-              y="52.768764">Y  </tspan></text>
-         <g
-            transform="matrix(0.43045322,0,0,0.43045322,289.9556,24.871143)"
-            id="g40300">
-           <path
-              fill="#f7931a"
-              d="M 63.033,39.744 C 58.759,56.887 41.396,67.32 24.251,63.045 7.113,58.771 -3.32,41.407 0.956,24.265 5.228,7.12 22.591,-3.314 39.731,0.96 56.875,5.234 67.307,22.6 63.033,39.744 Z"
-              id="path40296" />
-           <path
-              fill="#ffffff"
-              d="M 46.103,27.444 C 46.74,23.186 43.498,20.897 39.065,19.37 l 1.438,-5.768 -3.511,-0.875 -1.4,5.616 c -0.923,-0.23 -1.871,-0.447 -2.813,-0.662 l 1.41,-5.653 -3.509,-0.875 -1.439,5.766 c -0.764,-0.174 -1.514,-0.346 -2.242,-0.527 l 0.004,-0.018 -4.842,-1.209 -0.934,3.75 c 0,0 2.605,0.597 2.55,0.634 1.422,0.355 1.679,1.296 1.636,2.042 l -1.638,6.571 c 0.098,0.025 0.225,0.061 0.365,0.117 -0.117,-0.029 -0.242,-0.061 -0.371,-0.092 l -2.296,9.205 c -0.174,0.432 -0.615,1.08 -1.609,0.834 0.035,0.051 -2.552,-0.637 -2.552,-0.637 l -1.743,4.019 4.569,1.139 c 0.85,0.213 1.683,0.436 2.503,0.646 l -1.453,5.834 3.507,0.875 1.439,-5.772 c 0.958,0.26 1.888,0.5 2.798,0.726 l -1.434,5.745 3.511,0.875 1.453,-5.823 c 5.987,1.133 10.489,0.676 12.384,-4.739 1.527,-4.36 -0.076,-6.875 -3.226,-8.515 2.294,-0.529 4.022,-2.038 4.483,-5.155 z m -8.022,11.249 c -1.085,4.36 -8.426,2.003 -10.806,1.412 l 1.928,-7.729 c 2.38,0.594 10.012,1.77 8.878,6.317 z m 1.086,-11.312 c -0.99,3.966 -7.1,1.951 -9.082,1.457 l 1.748,-7.01 c 1.982,0.494 8.365,1.416 7.334,5.553 z"
-              id="path40298" />
-         </g>
-         <g
-            id="g107183"
-            transform="matrix(13.235206,0,0,13.235206,-1.061414e-6,-0.3491175)">
-           <rect
-              style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:1.0654"
-              id="rect45449"
-              width="0.91072226"
-              height="0.85631996"
-              x="4.0402594"
-              y="0.026377942" />
-           <rect
-              style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:1.863"
-              id="rect45553"
-              width="0.91072226"
-              height="2.6184042"
-              x="4.0396781"
-              y="1.3700036" />
-         </g>
-         <g
-            id="g107189"
-            transform="matrix(13.235206,0,0,13.235206,117.13949,-0.3491175)">
-           <rect
-              style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:1.0654"
-              id="rect107185"
-              width="0.91072226"
-              height="0.85631996"
-              x="4.0402594"
-              y="0.026377942" />
-           <rect
-              style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:1.863"
-              id="rect107187"
-              width="0.91072226"
-              height="2.6184042"
-              x="4.0396781"
-              y="1.3700036" />
-         </g>
-         <g
-            id="g107195"
-            transform="matrix(13.235206,0,0,13.235206,175.88502,-0.3491175)">
-           <rect
-              style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:1.0654"
-              id="rect107191"
-              width="0.91072226"
-              height="0.85631996"
-              x="4.0402594"
-              y="0.026377942" />
-           <rect
-              style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:1.863"
-              id="rect107193"
-              width="0.91072226"
-              height="2.6184042"
-              x="4.0396781"
-              y="1.3700036" />
-         </g>
-         <text
-            xml:space="preserve"
-            style="font-size:71.3091px;line-height:1.25;font-family:'The Bold Font';-inkscape-font-specification:'The Bold Font';stroke-width:1.78273"
-            x="245.08585"
-            y="52.198311"
-            id="text123857"><tspan
-              id="tspan123855"
-              style="stroke-width:1.78273"
-              x="245.08585"
-              y="52.198311">R  </tspan></text>
-       </g>
-     </svg>
+        <svg width="451" height="75" version="1.1" viewBox="0 0 317.5 52.769" xmlns="http://www.w3.org/2000/svg">
+     <g fill="#ffffff" fontFamily="The Bold Font" fontSize="71.309px" strokeWidth="1.7827">
+      <text x="185.955" y="52.554855" style={{ lineHeight: '1.25' }}>
+        <tspan x="185.955" y="52.554855" fill="#ffffff" strokeWidth="1.7827">B  </tspan>
+      </text>
+      <text x="69.28112" y="52.412239" style={{ lineHeight: '1.25' }}>
+        <tspan x="69.28112" y="52.412239" fill="#ffffff" strokeWidth="1.7827">RM  </tspan>
+      </text>
+      <text x="0.1426184" y="52.768764" style={{ lineHeight: '1.25' }}>
+        <tspan x="0.1426184" y="52.768764" fill="#ffffff" strokeWidth="1.7827">Y  </tspan>
+      </text>
+    </g>
+ <g transform="matrix(.43045 0 0 .43045 289.96 24.871)">
+  <path d="M 63.033,39.744 C 58.759,56.887 41.396,67.32 24.251,63.045 7.113,58.771 -3.32,41.407 0.956,24.265 5.228,7.12 22.591,-3.314 39.731,0.96 56.875,5.234 67.307,22.6 63.033,39.744 Z" fill="#f7931a"/>
+  <path d="m46.103 27.444c0.637-4.258-2.605-6.547-7.038-8.074l1.438-5.768-3.511-0.875-1.4 5.616c-0.923-0.23-1.871-0.447-2.813-0.662l1.41-5.653-3.509-0.875-1.439 5.766c-0.764-0.174-1.514-0.346-2.242-0.527l4e-3 -0.018-4.842-1.209-0.934 3.75s2.605 0.597 2.55 0.634c1.422 0.355 1.679 1.296 1.636 2.042l-1.638 6.571c0.098 0.025 0.225 0.061 0.365 0.117-0.117-0.029-0.242-0.061-0.371-0.092l-2.296 9.205c-0.174 0.432-0.615 1.08-1.609 0.834 0.035 0.051-2.552-0.637-2.552-0.637l-1.743 4.019 4.569 1.139c0.85 0.213 1.683 0.436 2.503 0.646l-1.453 5.834 3.507 0.875 1.439-5.772c0.958 0.26 1.888 0.5 2.798 0.726l-1.434 5.745 3.511 0.875 1.453-5.823c5.987 1.133 10.489 0.676 12.384-4.739 1.527-4.36-0.076-6.875-3.226-8.515 2.294-0.529 4.022-2.038 4.483-5.155zm-8.022 11.249c-1.085 4.36-8.426 2.003-10.806 1.412l1.928-7.729c2.38 0.594 10.012 1.77 8.878 6.317zm1.086-11.312c-0.99 3.966-7.1 1.951-9.082 1.457l1.748-7.01c1.982 0.494 8.365 1.416 7.334 5.553z" fill="#fff"/>
+ </g>
+ <g transform="matrix(13.235 0 0 13.235 -1.0614e-6 -.34912)" fill="#fff">
+  <rect x="4.0403" y=".026378" width=".91072" height=".85632"/>
+  <rect x="4.0397" y="1.37" width=".91072" height="2.6184"/>
+ </g>
+ <g transform="matrix(13.235 0 0 13.235 117.14 -.34912)" fill="#fff">
+  <rect x="4.0403" y=".026378" width=".91072" height=".85632"/>
+  <rect x="4.0397" y="1.37" width=".91075" height="2.6184"/>
+ </g>
+ <g transform="matrix(13.235 0 0 13.235 175.89 -.34912)" fill="#fff">
+  <rect x="4.0403" y=".026378" width=".91072" height=".85632"/>
+  <rect x="4.0397" y="1.37" width=".91072" height="2.6184"/>
+ </g>
+ <text x="245.08585" y="52.198311" fill="#ffffff" fontFamily="'The Bold Font'" fontSize="71.309px" strokeWidth="1.7827" style={{ lineHeight: '1.25' }}><tspan x="245.08585" y="52.198311" fill="#ffffff" strokeWidth="1.7827">R  </tspan></text>
+</svg>
     );
   };
-  
